7ab83f94beacb2df61158ce21378e0ea8e011f1c
Antoine Pelletier <antoine.pelletier@inrae.fr>
2021-01-15 12:40:25 +0000

    version 0.12.3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # baseflow
 
-This package computes hydrograph separation using the conceptual automated process from Pelletier and Andreassian, 2019. The present README file describes installation instructions.
+This package computes hydrograph separation using the conceptual automated process from [Pelletier and Andréassian, 2020](https://doi.org/10.5194/hess-24-1171-2020). It was developped by the [HYDRO research team](https://webgr.inrae.fr), part of the [HYCAR research unit](https://www6.jouy.inrae.fr/hycar/) and [INRAE institute](https://www.inrae.fr).
 
 *baseflow* is a R package whose computing core is made of a Rust library. Integration was realized through [RustR](https://github.com/rustr/rustr) API and [rustinr](https://github.com/rustr/rustinr) R package.
 
--- tools/staticlib.R
+++ tools/staticlib.R
@@ -12,22 +12,24 @@ osType <- function() {
   if ( ( ! grepl("^x86", info["machine"]) ) || ( ! ( sysname %in% c("Windows","Darwin","Linux") ) ) ) sprintf("%s-%s",info["sysname"],info["machine"])
   else if ( sysname == "Windows" ) "windows"
   else if ( sysname == "Darwin" ) "macosx"
+  else if ( sysname == "Darwin-arm64") "macosx-arm"
   else if ( sysname == "Linux" ) "linux"
   else sysname
 }
 osType <- osType()
 
-if(!(osType %in% c('windows', 'macosx', 'linux'))){
+if(!(osType %in% c('windows', 'macosx', 'linux', 'macosx-arm'))){
   cat(paste0(osType, ' is not a supported platform.\n',
-             'Please use baseflow package with Windows, GNU/Linux or Mac OS.\n'))
-  quit(status = 1)
+             'Compilation and installation may be possible if the Rust compiler is available.\n',
+             'It is recommended to use baseflow package with Windows, GNU/Linux or macOS.\n'))
+  #quit(status = 1)
 }
 
 # Detecting cargo installed version
 requiredCargoVersion <- "1.42.0"
 cat(paste0("Cargo version ", requiredCargoVersion, " or newer is required for compilation.\n"))
 
-if(!cargo_is_found && (osType == 'linux')){
+if(!cargo_is_found && !(osType %in% c("windows", "macosx", "macosx-arm"))){
   cat("Cargo is a requirement to compile the Rust library. Please visit https://rustup.rs to install it. You do not need admin rights.\n")
   quit(status = 1)
 }
@@ -35,7 +37,7 @@ if(cargo_is_found && (osType == 'windows')){
   installedCargoVersion <- gsub("cargo ([^ ]+).*", "\\1", system2("cargo","--version",stdout=TRUE))
   cat(paste0("Cargo version ", installedCargoVersion, " found.\n"))
   installedCargoVersion <- strsplit(installedCargoVersion, '-')[[1]][1]
-  is_installed_newer <- (compareVersion(installedCargoVersion, requiredCargoVersion) > 0)
+  is_installed_newer <- (compareVersion(installedCargoVersion, requiredCargoVersion) >= 0)
   
   # Compiling if cargo version is OK
   if(is_installed_newer){
@@ -51,7 +53,7 @@ if(cargo_is_found && (osType != 'windows')){
   installedCargoVersion <- gsub("cargo ([^ ]+).*", "\\1", system2("cargo","--version",stdout=TRUE))
   cat(paste0("Cargo version ", installedCargoVersion, " found.\n"))
   installedCargoVersion <- strsplit(installedCargoVersion, '-')[[1]][1]
-  is_installed_newer <- (compareVersion(installedCargoVersion, requiredCargoVersion) > 0)
+  is_installed_newer <- (compareVersion(installedCargoVersion, requiredCargoVersion) >= 0)
   
   # Compiling if cargo version is OK
   if(is_installed_newer){
@@ -72,6 +74,24 @@ if(cargo_is_found && (osType != 'windows')){
 # Using pre-compiled shared library on Windows and MacOS
 if(!cargo_is_found && (osType == 'macosx')){
   target <- "x86_64-apple-darwin"
+  cat("Downloading pre-compiled Rust library.\n")
+  download.file('https://gitlab.irstea.fr/HYCAR-Hydro/baseflow/-/raw/master/tools/tools.tar.gz',
+                destfile = 'tools.tar.gz')
+  dir.create('./ext_tools')
+  untar('tools.tar.gz', list = FALSE, exdir = './ext_tools')
+  
+  destDir <- "rustlib/target/release"
+  dir.create(destDir, recursive = TRUE)
+  
+  files_list <- c('librustlib.d', 'librustlib.rlib', 'librustlib.a')
+  for(f in files_list){
+    file.copy(from = paste0('./ext_tools/', target, '/release/', f),
+              to = paste0(destDir, '/', f), overwrite = TRUE)
+    file.remove(paste0('./ext_tools/', target, '/release/', f))
+  }
+}
+if(!cargo_is_found && (osType == 'macosx-arm')){
+  target <- "aarch64-apple-darwin"
   cat("Downloading pre-compiled Rust library.\n")
   download.file('https://gitlab.irstea.fr/HYCAR-Hydro/baseflow/-/raw/master/tools/tools.tar.gz',
                 destfile = 'tools.tar.gz')
